2daaf3a98e83baac0b71ff2c2d074bb3e3e4b184
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-01 10:18:49 -0500

    Fixed dumb error in urls.

--- soundscapes-app/soundscapes/urls.py
+++ soundscapes-app/soundscapes/urls.py
@@ -6,5 +6,5 @@ from episodes.views import (ShowListView, ShowCreateView, ShowDetailView)
 urlpatterns = patterns('',
     url(r'^shows/$', ShowListView.as_view(), name = 'show_list'),
     url(r'^shows/new/$', ShowCreateView.as_view(), name = 'new_show'),
-    url(r'^shows/(<?pk>\d+)/$', ShowDetailView.as_view(), name = 'view_show'),
+    url(r'^shows/(?P<pk>\d+)/$', ShowDetailView.as_view(), name = 'view_show'),
 )
