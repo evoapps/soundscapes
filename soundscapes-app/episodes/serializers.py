7ccd1174e50edc420b7287735e5eb95df112f731
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-13 19:22:06 -0500

    Use REST framework for model serialization

---
+++ soundscapes-app/episodes/serializers.py
@@ -0,0 +1,17 @@
+
+from rest_framework import serializers
+
+from .models import Episode, Segment
+
+class SegmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Segment
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    segment_set = SegmentSerializer(many = True)
+
+    class Meta:
+        model = Episode
+        fields = ('id', 'show', 'released', 'title', 'mp3', 'segment_set')
+        depth = 1
