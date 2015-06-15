eb29d38fc242208a726147bbc8fcc732c148cea7
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-15 08:22:11 -0500

    Render episode list as list of divs with href attr
    Warning: Removes previous D3 code that drew the
    episodes as <g><text>...</text></g> on an <svg>
    canvas. Simplifying to <div>s should make it
    easier to focus on the more difficult part of the
    visualization: drawing each episode's segments.

--- soundscapes-app/episodes/static/episodes/js/soundscapes.js
+++ soundscapes-app/episodes/static/episodes/js/soundscapes.js
@@ -1,95 +1,23 @@
 
-show_soundscapes = function(episodes) {
-  var episodes = JSON.parse(episodes); // don't know why d3.json doesn't handle this
+function drawEpisodeList(episodes) {
+  globalVars.episodes = episodes;
 
   // Convert release string to javascript Date
-  episodes.forEach(function(el) {
-    el.fields.released = Date.parse(el.fields.released);
+  episodes.forEach(function (ep) {
+    ep.released = Date.parse(ep.released);
   });
 
-  raw = episodes; // save for poking around on the console
-
-  var numEpisodes = episodes.length,
-      episodeDetailHeight = 30,
-      episodeTimelineRadius = 5;
-
-  var svgWidth = 700,
-      svgHeight = episodeDetailHeight * numEpisodes;
-
-  var episodeDetailX = 100,
-      episodeTimelineX = 20;
-
-  var episodeDetailScale = d3.scale.linear()
-    .domain([episodes.length, 1])
-    .range([svgHeight - episodeDetailHeight, episodeDetailHeight]);
-
-  var episodeTimelineScale = d3.time.scale()
-    .domain([
-      d3.min(episodes, function(el) { return el.fields.released; }),
-      Date.now()
-    ])
-    .range([svgHeight - episodeTimelineRadius, episodeTimelineRadius]);
-
-  var hover = function(el, bool) { d3.select(this).classed("hover", bool); },
-      hoverOn = function(el) { return hover(el, true); },
-      hoverOff = function(el) { return hover(el, false); };
-
-  d3.select("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight)
-
-  highlight = function(el) {
-    var gEpisode = d3.select(this);
-    gEpisode.select("text").classed("hover", true);
-    gEpisode.select("circle").classed("hover", true);
-    gEpisode.select("path").classed("hover", true);
-  }
-
-  unlight = function(el) {
-    var gEpisode = d3.select(this);
-    gEpisode.select("text").classed("hover", false);
-    gEpisode.select("circle").classed("hover", false);
-    gEpisode.select("path").classed("hover", false);
-  }
-
-  d3.select("svg")
-    .selectAll("g.episode")
+  d3.select("#episodeList")
+    .selectAll("div.episode")
     .data(episodes)
     .enter()
-    .append("g")
-    .attr("class", "episode")
-    .on("mouseover", highlight)
-    .on("mouseout", unlight);
+    .append("div")
+    .attr("class", "episode");
 
-  redirectToEpisodeDetail = function(episodePk) {
-    window.location.href = episodeDetailURLRouter(episodePk);
-  }
+  var episodeDivs = d3.selectAll("div.episode");
 
-  d3.selectAll("g.episode")
-    .append("text")
-    .text(function(ep) { return ep.fields.title; })
-    .attr("x", episodeDetailX)
-    .attr("y", function(el, i) { return episodeDetailScale(i); })
-    .on("click", function(el) {
-      if !(el.fields.mp3) {
-        downloadEpisode(el.pk);
-      }
-
-      redirectToEpisodeDetail(el.pk);
-    });
-
-  d3.selectAll("g.episode")
-    .append("circle")
-    .attr("r", episodeTimelineRadius)
-    .attr("cx", episodeTimelineX)
-    .attr("cy", function(el) { return episodeTimelineScale(el.fields.released); });
-
-  var connector = d3.svg.diagonal.radial()
-    .source(function(el) { return {x: episodeTimelineX, y: episodeTimelineScale(el.fields.released)}; })
-    .target(function(el, i) { return {x: episodeDetailX, y: episodeDetailScale(i)}; });
-
-  d3.selectAll("g.episode")
-    .append("path")
-    .attr("d", connector)
-    .attr("class", "link");
+  episodeDivs.append("h2")
+    .append("a")
+    .attr("href", function (ep) { return ep.url; })
+    .text(function (ep) { return ep.title; });
 }
