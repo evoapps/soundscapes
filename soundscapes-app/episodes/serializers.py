c86c81772411f238682978436888d6e8bd1a2b85
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-18 06:56:08 -0500

    Implement Moments in Episode Serializers

--- soundscapes-app/episodes/serializers.py
+++ soundscapes-app/episodes/serializers.py
@@ -2,12 +2,19 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
-from .models import Show, Episode, Segment
+from .models import Show, Episode, Moment, Segment
+
+class MomentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Moment
 
 class SegmentSerializer(serializers.ModelSerializer):
+    moments = MomentSerializer(many = True)
 
     class Meta:
         model = Segment
+        fields = ('id', 'start_time', 'end_time', 'moments')
 
 class EpisodeSerializer(serializers.ModelSerializer):
     segments = SegmentSerializer(many = True)
--- soundscapes-app/episodes/tests/test_serializers.py
+++ soundscapes-app/episodes/tests/test_serializers.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from episodes.models import Episode
+from episodes.models import Episode, Moment, Segment
 from episodes.serializers import EpisodeSerializer
 
 class SerializerTest(TestCase):
@@ -12,3 +12,20 @@ class SerializerTest(TestCase):
         serializer = EpisodeSerializer(episode)
         data = serializer.data
         self.assertIn('url', data.keys())
+
+    def test_serialized_episode_has_moment_data(self):
+        num_moments = 10
+        episode = mommy.make(Episode)
+        moments = mommy.make(Moment, episode = episode, _quantity = num_moments)
+        segment = mommy.make(Segment, episode = episode, moments = moments)
+
+        serializer = EpisodeSerializer(episode)
+        data = serializer.data
+
+        self.assertIn('segments', data.keys())
+        segment_data = data['segments'][0]
+
+        self.assertIn('moments', segment_data.keys())
+        segment_moment_data = segment_data['moments'][0]
+        self.assertIn('time', segment_moment_data)
+        self.assertIn('value', segment_moment_data)
