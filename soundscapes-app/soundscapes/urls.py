dde440a8c768f9e77ec70ad49980b04c78065b1a
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-20 15:06:38 -0500

    Serve media files during development.

--- soundscapes-app/soundscapes/urls.py
+++ soundscapes-app/soundscapes/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from episodes import views
@@ -44,4 +46,4 @@ urlpatterns = patterns('',
         views.download_episode,
         name = 'download_episode',
     ),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
