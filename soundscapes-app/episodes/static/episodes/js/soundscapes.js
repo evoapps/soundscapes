e837d0639e62f4974e5d140616f91c599fab9014
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-20 07:52:49 -0500

    Show soundscapes on the correct axis

--- soundscapes-app/episodes/static/episodes/js/soundscapes.js
+++ soundscapes-app/episodes/static/episodes/js/soundscapes.js
@@ -20,11 +20,19 @@ function drawEpisodeList(episodes) {
     .text(function (ep) { return ep.title; });
 }
 
+function addEndMoments(segment) {
+  var firstMoment = {time: segment.start_time, value: 0.0},
+      lastMoment = {time: segment.end_time, value: 0.0};
+  segment.moments.splice(0, 0, firstMoment);
+  segment.moments.push(lastMoment);
+}
+
 function drawSegments(episode) {
   // don't know if this is going to be a single object or an array
   globalVars.episode = episode;
 
   parseEpisode(episode);
+  episode.segments.forEach(addEndMoments);
 
   var svgWidth = 500,
       svgHeight = 200;
@@ -39,11 +47,11 @@ function drawSegments(episode) {
 
   timeScale
     .domain(d3.extent(episode.moments, function (moment) { return parseFloat(moment.time); }))
-    .range([0, svgWidth])
+    .range([0, svgWidth]);
 
   valueScale
     .domain(d3.extent(episode.moments, function (moment) { return moment.value; }))
-    .range([0, svgHeight])
+    .range([svgHeight, 0]);
 
   d3.select("svg")
     .attr("width", svgWidth)
@@ -53,7 +61,7 @@ function drawSegments(episode) {
     .enter()
     .append("path")
     .attr("class", "segment")
-    .attr("d", function (segment) { return line(segment.moments); })
+    .attr("d", function (segment) { return line(segment.moments) + "Z"; })
 }
 
 function parseEpisode(episode) {
