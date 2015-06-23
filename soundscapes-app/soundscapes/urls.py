e56cdc44ae570d60a14babedbf24604ae6ab020d
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-23 17:03:20 -0500

    Added simple template for home page

--- soundscapes-app/soundscapes/urls.py
+++ soundscapes-app/soundscapes/urls.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from episodes import views
 
 urlpatterns = patterns('',
+    url(
+        r'^$',
+        TemplateView.as_view(template_name = 'home.html'),
+    ),
     url(
         r'^shows/$',
         views.ShowListView.as_view(),
