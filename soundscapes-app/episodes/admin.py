890ce459ae7cc5f6c7d778d0a0b2a6ff12606f8f
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-23 14:11:51 -0500

    Admin action to download selected episodes

--- soundscapes-app/episodes/admin.py
+++ soundscapes-app/episodes/admin.py
@@ -12,9 +12,17 @@ refresh.short_description = "Refresh episode feed"
 class ShowAdmin(admin.ModelAdmin):
     actions = [refresh, ]
 
+
+def download(modeladmin, request, queryset):
+    for episode in queryset:
+        episode.download()
+download.short_description = "Download the episode"
+
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
     list_filter = ('show', )
+    actions = [download, ]
+
 
 @admin.register(Segment)
 class SegmentAdmin(admin.ModelAdmin):
