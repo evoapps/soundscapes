3b18d38d5ca3a61085e9f8f116ad282f10a4b769
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-23 11:00:16 -0500

    Register default model admin models

--- soundscapes-app/episodes/admin.py
+++ soundscapes-app/episodes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Show, Episode, Segment
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    pass
