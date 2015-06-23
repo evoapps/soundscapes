854cd05b8def5db1b26e54954cc9fbe7b3d9f362
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-23 10:52:00 -0500

    Include admin site urls

--- soundscapes-app/soundscapes/urls.py
+++ soundscapes-app/soundscapes/urls.py
@@ -46,4 +46,5 @@ urlpatterns = patterns('',
         views.download_episode,
         name = 'download_episode',
     ),
+    url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
