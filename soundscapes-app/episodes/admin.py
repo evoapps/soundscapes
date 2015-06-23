6aa5e9807387bf085856af7432dfc1566e5fa174
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-23 11:02:27 -0500

    Custom admin action to refresh episodes

--- soundscapes-app/episodes/admin.py
+++ soundscapes-app/episodes/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from .models import Show, Episode, Segment
 
+
+def refresh(modeladmin, request, queryset):
+    for show in queryset:
+        show.refresh()
+refresh.short_description = "Refresh episode feed"
+
 @admin.register(Show)
 class ShowAdmin(admin.ModelAdmin):
-    pass
+    actions = [refresh, ]
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
