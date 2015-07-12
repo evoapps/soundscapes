a08871c09a02227560eb4cb8bcc84cff91c6fad8
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-07-11 23:30:23 -0500

    Rolled all js into one file

--- episodes/static/episodes/js/soundscapes.js
+++ episodes/static/episodes/js/soundscapes.js
@@ -125,3 +125,26 @@ function addEndMoments(segment, minimum) {
   segment.moments.splice(0, 0, firstMoment);
   segment.moments.push(lastMoment);
 }
+
+
+function updateProgress() {
+  var audioElement = document.querySelector("audio");
+
+  currentTime = audioElement.currentTime;
+  console.log(timeScale(currentTime));
+
+  d3.select("#needle")
+    .attr("x1", timeScale(currentTime))
+    .attr("x2", timeScale(currentTime));
+}
+
+function toggleAudio() {
+  var audioElement = document.querySelector("audio");
+
+  if (audioElement.paused) {
+    audioElement.play();
+  } else {
+    audioElement.pause();
+  }
+
+}
