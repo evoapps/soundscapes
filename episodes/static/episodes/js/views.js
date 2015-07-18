5663404f7c52ee4263ec8bec4aedb56bdadbccc3
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-07-18 16:04:01 -0500

    Stashing for now. Need to figure out how to handle d3 settings

--- episodes/static/episodes/js/views.js
+++ episodes/static/episodes/js/views.js
@@ -1,13 +1,29 @@
+
+d3settings = new D3Settings();
+
 var EpisodeView = Backbone.View.extend({
   tagName: "svg",
+  initialize: function () {
+    console.log(this.model);
+
+    d3
+  },
   render: function () {
     var episode = this.model.attributes;
 
-    d3.select(this.el)
+    // Set svg attributes for this episode
+    var svg = d3.select(this.el);
+
+    var width = 600, //d3.max(d3settings.timeScale.range()),
+        height = 400; // d3.max(valueScale.range());
+
+    svg
+      .attr("width", width)
+      .attr("height", height);
+
+    svg
       .append("text")
       .text(episode.title);
-
-
   }
 })
 
