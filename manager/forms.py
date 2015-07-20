c0e47d28d56e99bb2600759a76210e76791e19f4
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-07-19 22:19:28 -0500

    Use crispy_forms and added manager navigation

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.urlresolvers import reverse
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 from .models import Show
 
@@ -7,6 +11,13 @@ class ShowForm(forms.ModelForm):
     class Meta:
         model = Show
         fields = ('name', 'slug', 'rss_url')
+
+    def __init__(self, *args, **kwargs):
+        super(ShowForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('show_create')
+        self.helper.add_input(Submit('submit', 'Submit'))
 
     def save(self, *args, **kwargs):
         show = super(ShowForm, self).save(*args, **kwargs)
