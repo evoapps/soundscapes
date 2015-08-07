b936676983136b3fffc4de59bdaf381c0614e53b
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-08-07 15:22:38 -0500

    List shows or episodes

--- manager/management/commands/list.py
+++ manager/management/commands/list.py
@@ -1,11 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from manager.models import Episode
+from manager.models import Episode, Show
 
 class Command(BaseCommand):
     help = 'List Episodes by id'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--shows', action = 'store_true', default = False)
+
     def handle(self, *args, **options):
-        for episode in Episode.objects.all():
-            kwargs = {'id': episode.id, 'title': episode.title}
-            self.stdout.write('{id}: {title}'.format(**kwargs))
+        if options['shows']:
+            for show in Show.objects.all():
+                kwargs = {'id': show.id, 'show': show.name}
+                self.stdout.write('{id}: {show}'.format(**kwargs))
+        else:
+            for episode in Episode.objects.all():
+                kwargs = {'id': episode.id, 'title': episode.title}
+                self.stdout.write('{id}: {title}'.format(**kwargs))
